0048378e9e41017f50b28e0424d647734aa84f81
AshyX-Dev <AshyXDev@gmail.com>
2025-03-10 17:14:39 +0330

    PHP - Handling chats

--- PHP/bot.php
+++ PHP/bot.php
@@ -43,12 +43,12 @@ function handleChats(): never {
                 if (isset($update['message'])){
                     if (!in_array($update['message']['message_id'], $msg_ids)) {
                         array_push($msg_ids, $update['message']['message_id']);
-                        if (in_array($update['message']['from']['id'], $msg_ids)){
-                            if (strpos($update['message']['text'], '/start')){
+                        if (in_array($update['message']['from']['id'], $admins)){
+                            if (strpos($update['message']['text'], '/start') === true){
                                 $bot->sendMessage([
                                     "text" => makeFont("welcome to Admin Panel 🛰\n\n🔰 /start - start bot\n📁 /port <...userid> <...mode>\n🕹 /ban <...userid>\n🎛 /unban <...userid>\n👁 /see <...userid> - see profile"),
                                     "chat_id" => $update['message']['chat']['id'],
-                                    "reply_to_message_id" => $update["message"]["id"]
+                                    "reply_to_message_id" => $update["message"]["message_id"]
                                 ]);
                             }
                         }
@@ -61,4 +61,33 @@ function handleChats(): never {
     }
 }
 
-handleChats();
+function handleChatsSec(): never {
+    $bot = new Ch3fPooler("YOUR TOKEN");
+    $admins = [000, 111, 222];
+    $msg_ids = [];
+
+    while (true) {
+        $updates = $bot->getUpdates();
+        if ($updates['local_status'] === "OK" && isset($updates['response']['ok']) && $updates['response']['ok']) {
+            foreach ($updates['response']['result'] as $update) {
+                if (isset($update['message']) && !in_array($update['message']['message_id'], $msg_ids)) {
+                    array_push($msg_ids, $update['message']['message_id']);
+                    if (in_array($update['message']['from']['id'], $admins)) {
+                        if (strpos($update['message']['text'], '/start') !== false) {
+                            $bot->sendMessage([
+                                "text" => makeFont("Welcome to Admin Panel 🛰\n\n🔰 /start - start bot\n📁 /port <...userid> <...mode>\n🕹 /ban <...userid>\n🎛 /unban <...userid>\n👁 /see <...userid> - see profile"),
+                                "chat_id" => $update['message']['chat']['id'],
+                                "reply_to_message_id" => $update["message"]["message_id"]
+                            ]);
+                        }
+                    }
+                }
+            }
+        }
+        // Optional: Add a sleep to prevent overwhelming the server
+        usleep(100000); // Sleep for 100 milliseconds
+    }
+}
+
+
+handleChatsSec();
